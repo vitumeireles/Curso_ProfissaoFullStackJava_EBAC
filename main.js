1cfe3a6b12098d6cc0dfe17273bf9b4a84067e66
vitumeireles <87585413+vitumeireles@users.noreply.github.com>
2024-03-16 17:17:22 -0300

    new file:   index.html 	new file:   main.css 	new file:   main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    // Adicionar tarefa
+    $('#task-form').submit(function(event) {
+        event.preventDefault();
+        var taskName = $('#task-input').val();
+        if (taskName.trim() !== '') {
+            $('#task-list').append('<li>' + "•" + " " + taskName + '</li>');
+            $('#task-input').val('');
+        }
+    });
+
+    // Marcar como concluído
+    $('#task-list').on('click', 'li', function() {
+        $(this).toggleClass('completed');
+    });
+});
