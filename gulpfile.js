f08679477efa1e2c0f172657184bb35382039d1d
vitumeireles <87585413+vitumeireles@users.noreply.github.com>
2024-03-23 18:39:36 -0300

    new file:   .DS_Store 	modified:   .gitignore 	modified:   gulpfile.js 	modified:   package-lock.json 	modified:   package.json 	new file:   source/.DS_Store 	new file:   source/images/bird-8232427_1280.jpg 	new file:   source/images/bird-8615360_1280.jpg 	new file:   source/images/dachshund-8120554_1280.jpg 	new file:   source/images/escalator-8229360_1280.jpg 	new file:   source/images/pier-7831868_1280.jpg 	new file:   source/images/road-7704729_1280.jpg 	renamed:    aritimetica.js -> source/scripts/aritimetica.js 	renamed:    sum.js -> source/scripts/sum.js 	new file:   source/styles/main.scss 	new file:   source/styles/variaveis.scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,35 @@
-function funcaoPadrao (callback) {
-    console.log("Executando via Gulp");
-    callback();
-}
-function dizOi (callback) {
-    console.log("Olá Gulp");
-    callback();
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+const obfuscate = require('gulp-obfuscate');
+const imagemin = require('gulp-imagemin');
+
+function comprimeImagens() {
+    return gulp.src ('./source/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./build/images'));
 }
 
-exports.default = funcaoPadrao;
-exports.dizOi = dizOi;
+function comprimeJavaScript() {
+    return gulp.src('./source/scripts/*.js')
+    .pipe(uglify())
+    .pipe(obfuscate())
+    .pipe(gulp.dest('./build/scripts'));
+}
+
+function compilaSass() {
+    return gulp.src('./source/styles/main.scss')
+    .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./build/styles'));
+}
+
+exports.default = function() {
+    gulp.watch('./source/styles/*.scss', { ignoreInitial: false }, gulp.series(compilaSass));
+    gulp.watch('./source/scripts/*.js', { ignoreInitial: false }, gulp.series(comprimeJavaScript));
+    gulp.watch('./source/images/*', { ignoreInitial: false }, gulp.series(comprimeImagens));
+}
